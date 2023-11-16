bb6710fcba62e59c673ff7b5653f2d549f4968dc
Nikolay Markhotskyi <marhotsky@gmail.com>
2023-11-16 23:52:48 +0200

    Change: routing to snake-game

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/game-snake">
+    <BrowserRouter basename="/snake-game">
       <App />
     </BrowserRouter>
   </React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/game-snake',
+  base: '/snake-game',
   server: {
-    open: '/game-snake',
+    open: '/snake-game',
     port: 3000,
   },
 });
